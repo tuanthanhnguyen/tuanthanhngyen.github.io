36ebea33561d932dd3bc098dc4080d519168a5bf
tuanthanhnguyen <63715128+tuanthanhnguyen@users.noreply.github.com>
2020-04-29 08:12:04 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+'''from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/")
+
+def home():
+
+    return render_template('home.html')
+
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)'''
+from flask import Flask, render_template 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/')
+def home():
+    return render_template("index.html")
+    return "Hey there,this is Thanh's website"
+@app.route('/about-me')
+def about():
+    return render_template('about.html')
+@app.route('/introduction')
+def about():
+    return render_template('hello.html')
+ 
+if __name__ == '__main__': app.run(debug=True) 
+ 
